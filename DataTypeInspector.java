831857993b456daef4961aea6a160f7c04fd87ef
Palishma <np01cp4a250034@islingtoncollege.edu.np>
2025-11-19 11:17:00 +0545

    changed

---
+++ DataTypeInspector.java
@@ -0,0 +1,23 @@
+
+public class DataTypeInspector
+{
+    public static void main(String[] args){
+    System.out.println("8 primitive Java data types: ");
+    int a=9;
+    byte b=109;
+    short c=32000;
+    long d= 15678;
+    float e= 40.34f;
+    double f=23.89;
+    boolean g= true;
+    char h= 'l';
+    System.out.println("byte b = " +b);
+    System.out.println("int a = " +a) ;
+    System.out.println("short c = " +c);
+    System.out.println("long d = " + d);
+    System.out.println("float e = " +e) ;
+    System.out.println("double f = " +f) ;
+    System.out.println("char h = " +h) ;
+    System.out.println("String g = " +g) ;
+}
+}
