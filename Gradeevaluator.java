72d68f4b97eb2645681ca9f9944cb158fed290dc
Palishma <np01cp4a250034@islingtoncollege.edu.np>
2025-11-19 11:04:28 +0545

    changed data

---
+++ Gradeevaluator.java
@@ -0,0 +1,9 @@
+
+public class Gradeevaluator
+{
+    public static void main(String[] args){
+    int grade=60;
+    String checking=(grade>=40) ? "Pass" : "Fail";
+    System.out.println("checking");
+    }
+}
