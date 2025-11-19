0116d6a5ff159ac0aa9bca3ef502718bddfe14ae
Palishma <np01cp4a250034@islingtoncollege.edu.np>
2025-11-19 10:31:03 +0545

    changed again

---
+++ Arithmetic.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Arithmetic here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Arithmetic
+{
+   public static void main (String[] args){
+       int a=2;
+       int b=3;
+       int arithmetic=a+b;
+       System.out.println(arithmetic);
+       System.out.println("a+b="+);
+       System.out.println(++a);
+       System.out.println(a!=b);
+       
+ 
+    }
+}   
